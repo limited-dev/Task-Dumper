0c6ca324bb7eefa11fab46e06629c95eedbba157
limited_dev <limited-dev@users.noreply.github.com>
2021-05-09 20:04:45 +0200

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,7 +1,7 @@
 @echo off
 title Taskdumper by limited_dev
 echo Dumping Tasks
-echo the follwing will be dumped:
+echo the following will be dumped:
 tasklist
 tasklist > Tasklist.txt
 echo Dumped Tasks to file "Tasklist.txt"
